3dbfb650e8493e6ec937ec97724b2062756540fd
GAURAV PATEL <magicgalaxy0@gmail.com>
2022-10-20 16:18:43 +0530

    email validation com

--- src/components/App.js
+++ src/components/App.js
@@ -9,14 +9,53 @@ import React, { useState, useRef } from 'react';
 
 function App() {
 
- /**
-  * code here
-  */
+  const [fname, setFname]= useState("");
+  const [lname, setLname] = useState("");
+  function data1(fname,lname) {
+    this.fname = fname;
+    this.lname = lname;
+  }
+  var data = new data1(fname,lname);
+
+  const handle_Submit=(e)=>{
+    e.preventDefault();
+
+    // function data1(fname,lname) {
+    //   this.fname = fname;
+    //   this.lname = lname;
+    // }
+
+    // var data = new data1(fname,lname);
+    // console.log(data.fname, data.lname, data)
+
+  }
+
+  const is_name_valid =()=>{
+    if(typeof fname === 'string' && fname.length > 1){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  const looksLikeMail=()=> {
+    var lastAtPos = lname.lastIndexOf("@");
+    var lastDotPos = lname.lastIndexOf(".");
+    return (
+      lastAtPos < lastDotPos &&
+      lastAtPos > 0 &&
+      lname.indexOf("@@") == -1 &&
+      lastDotPos > 2 &&
+      lname.length - lastDotPos > 2
+    );
+  }
 
   return(
     <div className="App">
+      <h1>hello</h1>
+
       <h1>How About Them Apples</h1>
-      <form>
+      {/* <form onSubmit={handle_Submit}>
         <fieldset>
           <label>
             <p>First Name</p>
@@ -29,6 +68,30 @@ function App() {
         </fieldset>
 
         <button id='submit' type="submit">Submit</button>
+      </form>
+      {
+        data.fname != undefined && (
+          <div>
+          <h1>{data.fname}</h1>
+          <h2>{data.lname}</h2>
+          </div>
+        )
+      } */}
+
+
+      <form onSubmit={handle_Submit}>
+        <fieldset>
+          <label>
+            <p>First Name</p>
+            <input id='fname' name="name" value={fname} onInput={(e)=>{setFname(e.target.value)}} />
+            <br></br>
+            <p>Email</p>
+            <input id='lname' name="name" value={lname} onInput={(e)=>{setLname(e.target.value)} }  />
+            {!looksLikeMail() && <h2 style={{color: 'red'}}>Email is invalid</h2>}
+          </label>
+        </fieldset>
+
+        <button id='submit' type="submit" disabled={!looksLikeMail()}>Submit</button>
       </form>
       {
         data.fname != undefined && (
